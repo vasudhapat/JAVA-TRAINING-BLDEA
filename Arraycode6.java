5dbc3ea8386b9d70d008652b4339dd44708202bc
New User <New User@DESKTOP-TL4QUCI>
2023-03-23 10:28:08 -0700

    Array code6

---
+++ Arraycode6.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package bldea_tiny;
+
+/**
+ * @author New User
+ *
+ */
+import java.util.Arrays;
+public class Arraycode6 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[]= {10,33,23,43,3,63,11,12};
+		Arrays.sort(arr);
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.print(arr[i]+",");
+		}
+				
+		}
+}
+
+	
